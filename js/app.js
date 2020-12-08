4f55ce82bb005a0782ff2699a329e6d70c282c3f
ReginaWangui <reginahwangui801@gmail.com>
2020-12-08 09:34:55 +0300

    added js file

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function mOver(obj) {
+  obj.innerHTML = "Hello Am Regina the person your looking for"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "dont be shy say Hi!"
+}
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
